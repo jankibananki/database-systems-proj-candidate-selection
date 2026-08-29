97d24fbd7cc28f7ca7afbd89e7aec98d4232b777
d4kic <dakic.d.aleksandar@gmail.com>
2026-08-29 21:02:42 +0200

    osnovna postavka i swagger

---
+++ treciProjekat/SelekcijaKandidataWebAPI/DataLayer.cs
@@ -0,0 +1,25 @@
+﻿using NHibernate;
+
+namespace SelekcijaKandidataWebAPI
+{
+    public class DataLayer
+    {
+        private static ISessionFactory? _factory;
+        private static readonly object ObjLock = new object();
+        private static string? _connectionString;
+
+        public static void Configure(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new ArgumentException("Connection string ne sme biti prazan.", nameof(connectionString));
+
+            lock (ObjLock)
+            {
+                if (_factory != null)
+                    throw new InvalidOperationException("DataLayer je vec inicijalizovan.");
+
+                _connectionString = connectionString;
+            }
+        }
+    }
+}
---
+++ treciProjekat/SelekcijaKandidataWebAPI/DataProvider.cs
@@ -0,0 +1,6 @@
+﻿namespace SelekcijaKandidataWebAPI
+{
+    public class DataProvider
+    {
+    }
+}
