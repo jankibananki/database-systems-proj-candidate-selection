fb33ad6d1acd88b5484031885a39fa4666afdfac
d4kic <dakic.d.aleksandar@gmail.com>
2026-08-30 15:28:20 +0200

    Dodavanje i brisanje oglasa

--- treciProjekat/SelekcijaKandidataWebAPI/Controllers/OglasController.cs
+++ treciProjekat/SelekcijaKandidataWebAPI/Controllers/OglasController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SelekcijaKandidata.Entiteti;
+using SelekcijaKandidataWebAPI.DTOs;
 
 namespace SelekcijaKandidataWebAPI.Controllers
 {
@@ -66,6 +68,76 @@ namespace SelekcijaKandidataWebAPI.Controllers
             {
                 var oglas = await DataProvider.VratiPraksuAsync(id);
                 return oglas == null ? NotFound() : Ok(oglas);
+            }
+            catch (Exception ex) 
+            { 
+                return BadRequest(ex.Message); 
+            }
+        }
+
+        [HttpPost("DodajStalniOglas")]
+        public async Task<IActionResult> DodajStalniOglas([FromBody] StalniOglasView oglas)
+        {
+            try 
+            {
+                await DataProvider.DodajStalniOglasAsync(oglas);
+                return Ok(oglas); 
+            }
+            catch (Exception ex) 
+            { 
+                return BadRequest(ex.Message); 
+            }
+        }
+
+        [HttpPost("DodajPrivremeniOglas")]
+        public async Task<IActionResult> DodajPrivremeniOglas([FromBody] PrivremeniOglasView oglas)
+        {
+            try 
+            { 
+                await DataProvider.DodajPrivremeniOglasAsync(oglas); 
+                return Ok(oglas);
+            }
+            catch (Exception ex) 
+            { 
+                return BadRequest(ex.Message); 
+            }
+        }
+
+        [HttpPost("DodajSezonskiOglas")]
+        public async Task<IActionResult> DodajSezonskiOglas([FromBody] SezonskiOglasView oglas)
+        {
+            try 
+            { 
+                await DataProvider.DodajSezonskiOglasAsync(oglas); 
+                return Ok(oglas);
+            }
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.Message); 
+            }
+        }
+
+        [HttpPost("DodajPraksu")]
+        public async Task<IActionResult> DodajPraksu([FromBody] PraksaView oglas)
+        {
+            try 
+            { 
+                await DataProvider.DodajPraksuAsync(oglas); 
+                return Ok(oglas); 
+            }
+            catch (Exception ex)
+            { 
+                return BadRequest(ex.Message); 
+            }
+        }
+
+        [HttpDelete("DeleteOglas/{id}")]
+        public async Task<IActionResult> DeleteOglas(int id)
+        {
+            try 
+            { 
+                await DataProvider.ObrisiOglasAsync(id); 
+                return Ok(); 
             }
             catch (Exception ex) 
             { 
--- treciProjekat/SelekcijaKandidataWebAPI/DataProvider.cs
+++ treciProjekat/SelekcijaKandidataWebAPI/DataProvider.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
+using NHibernate;
+using NHibernate.Linq;
 using SelekcijaKandidata.Entiteti;
 using SelekcijaKandidataWebAPI.DTOs;
 
@@ -85,6 +87,178 @@ namespace SelekcijaKandidataWebAPI
                 s = DataLayer.GetSession();
                 Praksa? oglas = await s.GetAsync<Praksa>(id);
                 return oglas == null ? null : new PraksaView(oglas);
+            }
+            finally
+            {
+                s?.Close();
+                s?.Dispose();
+            }
+        }
+
+        public static async Task DodajStalniOglasAsync(StalniOglasView view)
+        {
+            NHibernate.ISession? s = null;
+
+            try
+            {
+                s = DataLayer.GetSession();
+                using ITransaction tx = s.BeginTransaction();
+
+                StalniOglas o = new()
+                {
+                    NazivPozicije = view.NazivPozicije ?? string.Empty,
+                    VrstaOglasa = "stalni rad",
+                    Opis = view.Opis ?? string.Empty,
+                    MinPlata = view.MinPlata,
+                    MaxPlata = view.MaxPlata,
+                    DatumObjave = view.DatumObjave,
+                    DatumZatvaranja = view.DatumZatvaranja,
+                    Status = view.Status ?? string.Empty
+                };
+
+                await s.SaveAsync(o);
+                await tx.CommitAsync();
+
+                view.Id = o.Id;
+            }
+            finally
+            {
+                s?.Close();
+                s?.Dispose();
+            }
+        }
+
+        public static async Task DodajPrivremeniOglasAsync(PrivremeniOglasView view)
+        {
+            NHibernate.ISession? s = null;
+
+            try
+            {
+                s = DataLayer.GetSession();
+                using ITransaction tx = s.BeginTransaction();
+
+                PrivremeniOglas o = new()
+                {
+                    NazivPozicije = view.NazivPozicije ?? string.Empty,
+                    VrstaOglasa = "privremeni rad",
+                    Opis = view.Opis ?? string.Empty,
+                    MinPlata = view.MinPlata,
+                    MaxPlata = view.MaxPlata,
+                    DatumObjave = view.DatumObjave,
+                    DatumZatvaranja = view.DatumZatvaranja,
+                    Status = view.Status ?? string.Empty,
+                    Projekat = view.Projekat ?? string.Empty,
+                    PeriodAngazovanja = view.PeriodAngazovanja ?? string.Empty
+                };
+
+                await s.SaveAsync(o);
+                await tx.CommitAsync();
+
+                view.Id = o.Id;
+            }
+            finally
+            {
+                s?.Close();
+                s?.Dispose();
+            }
+        }
+
+        public static async Task DodajSezonskiOglasAsync(SezonskiOglasView view)
+        {
+            NHibernate.ISession? s = null;
+
+            try
+            {
+                s = DataLayer.GetSession();
+                using ITransaction tx = s.BeginTransaction();
+
+                SezonskiOglas o = new()
+                {
+                    NazivPozicije = view.NazivPozicije ?? string.Empty,
+                    VrstaOglasa = "sezonski rad",
+                    Opis = view.Opis ?? string.Empty,
+                    MinPlata = view.MinPlata,
+                    MaxPlata = view.MaxPlata,
+                    DatumObjave = view.DatumObjave,
+                    DatumZatvaranja = view.DatumZatvaranja,
+                    Status = view.Status ?? string.Empty,
+                    Sezona = view.Sezona ?? string.Empty,
+                    Lokacija = view.Lokacija ?? string.Empty
+                };
+
+                await s.SaveAsync(o);
+                await tx.CommitAsync();
+
+                view.Id = o.Id;
+            }
+            finally
+            {
+                s?.Close();
+                s?.Dispose();
+            }
+        }
+
+        public static async Task DodajPraksuAsync(PraksaView view)
+        {
+            NHibernate.ISession? s = null;
+
+            try
+            {
+                s = DataLayer.GetSession();
+                using ITransaction tx = s.BeginTransaction();
+
+                Praksa o = new()
+                {
+                    NazivPozicije = view.NazivPozicije ?? string.Empty,
+                    VrstaOglasa = "praksa",
+                    Opis = view.Opis ?? string.Empty,
+                    MinPlata = view.MinPlata,
+                    MaxPlata = view.MaxPlata,
+                    DatumObjave = view.DatumObjave,
+                    DatumZatvaranja = view.DatumZatvaranja,
+                    Status = view.Status ?? string.Empty,
+                    TrajanjeMeseci = view.TrajanjeMeseci,
+                    Mentor = await s.LoadAsync<Zaposleni>(view.IdMentora)
+                };
+
+                await s.SaveAsync(o);
+                await tx.CommitAsync();
+
+                view.Id = o.Id;
+            }
+            finally
+            {
+                s?.Close();
+                s?.Dispose();
+            }
+        }
+
+        public static async Task ObrisiOglasAsync(int id)
+        {
+            NHibernate.ISession? s = null;
+
+            try
+            {
+                s = DataLayer.GetSession();
+                using ITransaction tx = s.BeginTransaction();
+
+                bool imaCV = await s.Query<CV>().AnyAsync(c => c.Oglas.Id == id);
+
+                if (imaCV)
+                    throw new Exception("Ne moze se obrisati oglas dok postoje prijavljeni kandidati.");
+
+                var zahtevi = await s.Query<ZahtevOglas>().Where(z => z.Id.Oglas.Id == id).ToListAsync();
+
+                foreach (var z in zahtevi)
+                    await s.DeleteAsync(z);
+
+                Oglas? oglas = await s.GetAsync<Oglas>(id);
+
+                if (oglas != null)
+                {
+                    await s.DeleteAsync(oglas);
+                    await tx.CommitAsync();
+                }
             }
             finally
             {
